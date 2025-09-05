ca8577a45765b04a8687eaaaf930998768000107
808louie <louie.v.bala@hawaii.gov>
2025-09-05 11:01:20 -1000

    3:20:00

---
+++ EComm/EComm/DataAccess/Repository.cs
@@ -0,0 +1,12 @@
+﻿using EComm.Models;
+
+namespace EComm.DataAccess
+{
+    public class Repository
+    {
+        public List<Product> GetAllProducts()
+        {
+            return new List<Product>();
+        }
+    }
+}
